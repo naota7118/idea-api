bbc0c537226d30625f345a21c343a6d84df74ddc
naota7118 <naota7118@gmail.com>
2020-11-14 08:15:21 +0900

    add name column to ideas migration file

--- db/migrate/20201113230935_create_ideas.rb
+++ db/migrate/20201113230935_create_ideas.rb
@@ -1,7 +1,7 @@
 class CreateIdeas < ActiveRecord::Migration[6.0]
   def change
     create_table :ideas do |t|
-        t.string :name, null: false
+      t.string :name, null: false
       t.timestamps
     end
   end

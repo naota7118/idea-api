ce6a454ea15204cf1fe4fdf8f7903ae24a2f3756
naota7118 <naota7118@gmail.com>
2020-11-14 21:05:45 +0900

    update categories_controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,19 +1,15 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :update, :destroy]
 
-  # GET /categories
-  # GET /categories.json
   def index
     @categories = Category.all
+    render json: @categories
   end
 
-  # GET /categories/1
-  # GET /categories/1.json
   def show
+    render json: @idea
   end
 
-  # POST /categories
-  # POST /categories.json
   def create
     @category = Category.new(category_params)
 
@@ -24,8 +20,6 @@ class CategoriesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /categories/1
-  # PATCH/PUT /categories/1.json
   def update
     if @category.update(category_params)
       render :show, status: :ok, location: @category
@@ -34,19 +28,15 @@ class CategoriesController < ApplicationController
     end
   end
 
-  # DELETE /categories/1
-  # DELETE /categories/1.json
   def destroy
     @category.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_category
       @category = Category.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def category_params
       params.require(:category).permit(:name)
     end
--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,20 +1,15 @@
 class IdeasController < ApplicationController
   before_action :set_idea, only: [:show, :update, :destroy]
 
-  # GET /ideas
-  # GET /ideas.json
   def index
     @ideas = Idea.order(created_at: :desc)
     render json: @ideas
   end
 
-  # GET /ideas/1
-  # GET /ideas/1.json
   def show
+    render json: @idea
   end
 
-  # POST /ideas
-  # POST /ideas.json
   def create
     @idea = Idea.new(idea_params)
 
@@ -25,8 +20,6 @@ class IdeasController < ApplicationController
     end
   end
 
-  # PATCH/PUT /ideas/1
-  # PATCH/PUT /ideas/1.json
   def update
     if @idea.update(idea_params)
       render :show, status: :ok, location: @idea
@@ -35,20 +28,16 @@ class IdeasController < ApplicationController
     end
   end
 
-  # DELETE /ideas/1
-  # DELETE /ideas/1.json
   def destroy
     @idea.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_idea
       @idea = Idea.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def idea_params
-      params.require(:idea).permit(:name)
+      params.require(:idea).permit(:body, :category_id)
     end
 end

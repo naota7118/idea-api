17242cac8dcc55468595e59968b87c3d5981c403
naota7118 <naota7118@gmail.com>
2020-11-14 19:56:55 +0900

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_14_093505) do
+ActiveRecord::Schema.define(version: 2020_11_14_105010) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

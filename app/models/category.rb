ab9139fba513d1aa9acc2eef547fe0db478cb882
naota7118 <naota7118@gmail.com>
2020-11-14 19:50:36 +0900

    run rails g scaffold categories name:string --force

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,2 @@
 class Category < ApplicationRecord
-  has_many :idea
-  has_many :idea_category
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories
   root 'ideas#index'
   resources :categories do
     resources :ideas

ed53e99bf7b50aff6eb303af2256d4bb55e6a6cc
naota7118 <naota7118@gmail.com>
2020-11-14 19:49:02 +0900

    make a relation between category.rb and idea.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  has_many :idea
+  has_many :idea_category
 end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,2 +1,4 @@
 class Idea < ApplicationRecord
+  belongs_to :category
+  has_many :idea_category
 end

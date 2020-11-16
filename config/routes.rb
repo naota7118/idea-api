868fe2de7156a8274706e791faa244f0c433beb7
naota7118 <naota7118@gmail.com>
2020-11-17 06:31:43 +0900

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  # root 'ideas#index'
-  namespace 'api' do
-    namespace 'v1' do
-      resources :categories do
-        resources :ideas 
-      end
-    end
-  end
+  resources :ideas 
 end

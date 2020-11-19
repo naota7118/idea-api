a25104df5376f24470ce993e8d6fda52cf70b159
naota7118 <naota7118@gmail.com>
2020-11-19 19:01:56 +0900

    commentout json.jbuilder

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -3,39 +3,64 @@ class IdeasController < ApplicationController
 
   def index
     @ideas = Idea.order(created_at: :desc)
-    render json: @ideas
+    render json: { id: @ideas.ids, name: @ideas.select("body") }
   end
 
   def show
     render json: @idea
   end
-
+  
   def create
-    @idea = Idea.new(idea_params)
-    # @category = Category.new(category_params)
-    if @idea.save
-      render :show, status: :created, location: @idea
-    else
-      render json: @idea.errors, status: :unprocessable_entity
-    end
 
-    # すでにそのcategory_nameがcategoriesテーブルのnameカラムに存在する場合
-    if Category.exists?(name: category_params[:name])
-      # 登録しようとしてるcategory名と一致するcategory名をcategoriesテーブルから検索して探す
-      # 見つけたcategory名のidを取得してideasテーブルに登録する
-      @category_id = Category.find_by(name: category_params[:name]).select('id')
-      
-    # まだそのcategory_nameがcategoriesテーブルのnameカラムに存在してない場合
-    else 
-      # 新たなcategoryとしてcategoriesテーブルに登録し、ideasテーブルに登録する
-      @idea = Idea.new(idea_params)
-      @category = Category.new(category_params)
-      if @idea.save
-        render :show, status: :created, location: @idea
-      else
-        render json: @idea.errors, status: :unprocessable_entity
-      end
+  binding.pry
+    @category = Category.new(category_params)
+    @idea = Idea.new(idea_params)
+
+    # if Category.exists?(name: @category.name)
+    #   @categoryId = Category.find_by(name: @category.name).select('id')
+    #   @idea.category_id = @categoryId
+
+    if @idea.save && @category.save
+      # ステータスコード201を返す
+      head :created
+    else
+      # ステータスコード422を返す（バリデーションエラー時）
+      head :unprocessable_entity
     end
+  binding.pry
+    # # すでにそのcategory_nameがcategoriesテーブルのnameカラムに存在する場合
+    # if Category.exists?(name: category_params[:name])
+
+    #   # 登録しようとしてるcategory名と一致するcategory名をcategoriesテーブルから検索して探す
+    #   @categoryId = Category.find_by(name: category_params[:name]).select('id')
+
+    #   @idea = Idea.new(idea_params)
+
+    #   if @idea.save && @categoryId.save
+    #     # ステータスコード201を返す
+    #     head :created
+    #   else
+    #     # ステータスコード422を返す（バリデーションエラー時）
+    #     head :unprocessable_entity
+    #   end
+
+    # # まだそのcategory_nameがcategoriesテーブルのnameカラムに存在してない場合
+    # else 
+
+    #   # 新たなcategoryとしてcategoriesテーブルに登録し、ideasテーブルに登録する
+    #   @category = Category.new(category_params)
+    #   @idea = Idea.new(idea_params)
+
+    #   if @category.save && @idea.save
+    #     # ステータスコード201を返す
+    #     head :created
+    #   else
+    #     # ステータスコード422を返す（バリデーションエラー時）
+    #     head :unprocessable_entity
+    #   end
+
+    # end
+
   end
 
   def update
@@ -51,16 +76,16 @@ class IdeasController < ApplicationController
   end
 
   private
-    def set_idea
-      @idea = Idea.find(params[:id])
-    end
+  def set_idea
+    @idea = Idea.find(params[:id])
+  end
 
-    def idea_params
-      params.require(:idea).permit(:body, :category_id)
-    end
+  def idea_params
+    params.require(:idea).permit(:body)
+  end
 
-    def category_params
-      params.require(:category).permit(:name)
-    end
+  def category_params
+    params.require(:category).permit(:name)
+  end
 
 end

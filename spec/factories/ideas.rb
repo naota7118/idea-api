a1571114007f3c8d47a884a5a94bb3d2664bc2d7
naota7118 <naota7118@gmail.com>
2020-11-20 11:30:51 +0900

    rspec test

--- spec/factories/ideas.rb
+++ spec/factories/ideas.rb
@@ -1,8 +1,16 @@
 FactoryBot.define do
 
   factory :idea do
-    body        {'body'}
-    category_id {'1'}
+    body        { '仕事のtoリストを作る' }
+    sequence(:category_id) { |n| "#{n}" }
+
+    after(:build) do |idea|
+      idea.category = FactoryBot.build(:category)
+    end
   end
-  
+
+  factory :category do
+    name     { '仕事'}
+  end
+
 end

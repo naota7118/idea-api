1ece562c2d28e4041065f15db8834be4f3a5cb69
naota7118 <naota7118@gmail.com>
2020-11-23 20:11:39 +0900

    add some vaildation test

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,9 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe Idea, type: :model do
+RSpec.describe Idea do
 
-  before do
-    @idea = build(:idea)
+  it "is valid widh a body, category_id" do
+    idea = Idea.new(
+      body: "アイディアA",
+      category_id: "1"
+    )
+    expect(idea).to be_valid
+  end
+
+  # 有効なファクトリを持つこと
+  it "has v valid factory" do
+    expect(FactoryBot.create(:idea)).to be_valid
+  end
+
+  # bodyがなければ無効な状態であること
+  it "is invalid without body" do
+    idea = FactoryBot.create(:idea, body: nil)
+    idea.valid?
+    expe
+    ct(idea.errors[:body]).to include("can't be blank")
+  end
+  # category_idがなければ無効な状態であること
+  it "is invalid without category_id" do
+    idea = FactoryBot.create(:idea, category_id: nil)
+    idea.valid?
+    expect(idea.errors[:category_id]).to include("can't be blank")
+  end
+
+  before(:each) do
+    @idea = create(:idea)
   end
 
   describe 'バリデーション' do
